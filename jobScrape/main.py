e8d483528f712a114f755a7ccec3c045a0d0bf9e
copipastaguy <narutoalf@yahoo.com>
2022-05-25 13:41:45 +0800

    update

--- jobScrape/main.py
+++ jobScrape/main.py
@@ -1,14 +1,26 @@
 # web scrape script for LinkedIn jobs
 # python jobs
+
 import requests
 from bs4 import BeautifulSoup
 import csv
+import datetime
+import re
+
+# user can key input for dynamic search
+# print("Job title your are looking for: ")
+user_job = input("Job title your are looking for: ").replace(" ", "").lower()
+
+now = datetime.datetime.now()
+formatDate = now.strftime("%x")
 
 
 def scrape():
-    html = requests.get(
-        "https://www.linkedin.com/jobs/search?keywords=python&location=Singapore&geoId=102454443&trk=public_jobs_jobs-search-bar_search-submit&currentJobId=3087806252&position=2&pageNum=0"
-    ).text
+    url = f"https://www.linkedin.com/jobs/search?keywords={user_job}&location=Singapore&geoId=102454443&trk=public_jobs_jobs-search-bar_search-submit&currentJobId=3087806252&position=2&pageNum=0"
+    # html = requests.get(
+    #     "https://www.linkedin.com/jobs/search?keywords=python&location=Singapore&geoId=102454443&trk=public_jobs_jobs-search-bar_search-submit&currentJobId=3087806252&position=2&pageNum=0"
+    # ).text
+    html = requests.get(url).text
     soup = BeautifulSoup(html, "lxml")
 
     # job = soup.find("li", class_="jobs-search-results__list-item occludable-update")
@@ -18,13 +30,11 @@ def scrape():
     )
 
     for job in jobs:
-        published_date = job.find("time").text.replace(" ", "")
-        if "hours" or "days" in published_date:
+        published_date = job.find("time").text.strip()
+        if "hours" in published_date:
             company_name = job.find("h4").text.strip()
             job_title = job.find("h3").text.strip()
             job_link = job.a["href"]
-
-            # data = [company_name, job_title, job_link]
 
             # print(f"Posted {published_date.strip()}")
             # print(f"Company: {company_name.strip()}")
@@ -33,11 +43,14 @@ def scrape():
 
             # print(" ")
             try:
-                with open("Jobs.csv", "a") as fopen:
+                with open(f"{user_job}.csv", "a") as fopen:
                     csv_writer = csv.writer(fopen)
-                    data = [f"""{company_name} \n {job_title} \n {job_link}"""]
+                    data = [
+                        f"{published_date} \n {company_name} \n {job_title} \n {job_link}"
+                    ]
 
                     csv_writer.writerow(data)
+
             except:
                 return False
 
@@ -52,5 +65,6 @@ def scrape():
 
 
 if __name__ == "__main__":
-    print("Starting job scrape . .")
+    print(f"Starting job scrape for {user_job} on {formatDate}. . .")
     scrape()
+    print(f"Jobs scrape done for {user_job} on LinkedIn")
