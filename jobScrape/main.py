b598623bec091eab023dc47b0b9d4e0b79fc0283
copipastaguy <narutoalf@yahoo.com>
2022-05-27 11:54:30 +0800

    added code for export to json
    unable to export proper format of json
    - append dictionaries to list
    - convert list to json
    - export

--- jobScrape/main.py
+++ jobScrape/main.py
@@ -1,55 +1,82 @@
 # web scrape script for LinkedIn jobs
 # python jobs
 
+# 26/05/2022
+# change output format from csv to json for clearer view of data
+# create a list of dictionaries to store key values of jobs
+
 import requests
 from bs4 import BeautifulSoup
 import csv
 import datetime
-import re
+import json
+
 
 # user can key input for dynamic search
 # print("Job title your are looking for: ")
-user_job = input("Job title your are looking for: ").replace(" ", "").lower()
-
-now = datetime.datetime.now()
-formatDate = now.strftime("%x")
 
 
 def scrape():
+
     url = f"https://www.linkedin.com/jobs/search?keywords={user_job}&location=Singapore&geoId=102454443&trk=public_jobs_jobs-search-bar_search-submit&currentJobId=3087806252&position=2&pageNum=0"
-    # html = requests.get(
-    #     "https://www.linkedin.com/jobs/search?keywords=python&location=Singapore&geoId=102454443&trk=public_jobs_jobs-search-bar_search-submit&currentJobId=3087806252&position=2&pageNum=0"
-    # ).text
+
     html = requests.get(url).text
+
+    # init Beautiful soup instance
     soup = BeautifulSoup(html, "lxml")
 
-    # job = soup.find("li", class_="jobs-search-results__list-item occludable-update")
     jobs = soup.find_all(
         "div",
         class_="base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card",
     )
 
+    # get individual data without for loop
+    # running for loop to get all data from the page
     for job in jobs:
         published_date = job.find("time").text.strip()
-        if "hours" in published_date:
+        matches = ["minutes", "hour", "hours", "day", "days"]
+
+        if any(x in published_date for x in matches):
             company_name = job.find("h4").text.strip()
             job_title = job.find("h3").text.strip()
             job_link = job.a["href"]
 
-            # print(f"Posted {published_date.strip()}")
-            # print(f"Company: {company_name.strip()}")
-            # print(f"Looking for: {job_title.strip()}")
-            # print(f"Job link: {job_link}")
+            # empty list
+            output = []
 
-            # print(" ")
+            # dictionary of key pair value
+            data = {
+                "Posted": f"{published_date}",
+                "Company name": f"{company_name}",
+                "Job Title": f"{job_title}",
+                "Link": f"{job_link}",
+            }
+            # print(data)
+            # for i in range
+
+            output.append(data)
+            # print(output)
+            print(json.dumps(output, indent=4))
+
             try:
-                with open(f"{user_job}.csv", "a") as fopen:
-                    csv_writer = csv.writer(fopen)
-                    data = [
-                        f"{published_date} \n {company_name} \n {job_title} \n {job_link}"
-                    ]
+                # export CSV format
+                # with open(f"{user_job}.csv", "a") as fopen:
+                #     csv_writer = csv.writer(fopen)
+                #     data = [
+                #         f"{published_date} \n {company_name} \n {job_title} \n {job_link}"
+                #     ]
 
-                    csv_writer.writerow(data)
+                #     csv_writer.writerow(data)
+
+                # export JSON format
+                # data in dictionary form
+
+                # 1 company data
+
+                json_data = json.dumps(data, indent=2)
+                # with open(f"{user_job}.json", "a") as fopen:
+
+                #     fopen.write(json_data)
 
             except:
                 return False
@@ -65,6 +92,10 @@ def scrape():
 
 
 if __name__ == "__main__":
+    user_job = input("Job title your are looking for: ").replace(" ", "").lower()
+    now = datetime.datetime.now()
+    formatDate = now.strftime("%x")
+
     print(f"Starting job scrape for {user_job} on {formatDate}. . .")
     scrape()
     print(f"Jobs scrape done for {user_job} on LinkedIn")
